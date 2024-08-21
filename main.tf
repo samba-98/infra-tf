6dea8dc2da09114d1abe239b2d4986767c5c8c07
samba-98 <122912842+samba-98@users.noreply.github.com>
2024-08-21 12:34:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ resource "google_storage_bucket" "my-bucket2" {
   public_access_prevention = "enforced"
 }
 
-resource "google_compute_instance" "instance" {
+resource "google_compute_instance" "instance-00" {
   project = "devops-automation-433111"
-  name         = "my-inst"
+  name         = "my-inst-00"
   machine_type = "e2-medium"
   zone         = "us-west1-b"
   tags = ["abc", "def"]
