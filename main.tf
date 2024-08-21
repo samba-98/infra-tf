09aea8a237bceb95393ce5db2a2ea262aeffe477
samba-98 <122912842+samba-98@users.noreply.github.com>
2024-08-21 12:43:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ resource "google_compute_instance" "sample-inst" {
   project = "devops-automation-433111"
   name         = "sample-inst"
   machine_type = "e2-medium"
+  region      = "us-west1"
   zone         = "us-west1-a"
   tags = ["abc", "def"]
 
