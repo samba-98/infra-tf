1617c026734382c6127183c8a1dc72de05b6f3e4
samba-98 <122912842+samba-98@users.noreply.github.com>
2024-08-21 13:09:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,28 @@ resource "google_storage_bucket" "my-bucket3" {
   force_destroy = true
   public_access_prevention = "enforced"
 }
+
+provider "google" {
+  project     = "devops-automation-433111"
+  region      = "us-west1"
+}
+
+resource "google_compute_instance" "instance1" {
+  name         = "my-inst-1"
+  machine_type = "e2-medium"
+  zone         = "us-west1-b"
+
+  tags = ["abc", "def"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+  network_interface {
+    network = "default"
+}
+}
