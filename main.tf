e19edb04a73f0c52c07d2b9a905d382ef19c274c
samba-98 <122912842+samba-98@users.noreply.github.com>
2024-08-21 11:22:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my-bucket" {
-  name          = "first-bucket"
+  name          = "first-bucket01-using-tf"
   project = "devops-automation-433111"
   location      = "US"
   force_destroy = true
